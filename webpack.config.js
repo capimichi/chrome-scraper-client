d6482c914238c7091d1d9f977927ee7cf0de17e8
Michele Capichioni <capimichi@gmail.com>
2024-02-26 21:34:27 +0100

    Update webpack.config.js to include VueLoaderPlugin and configure module rules

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,58 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const {VueLoaderPlugin} = require('vue-loader');
+
 
 module.exports = {
     mode: 'production', // 'development' or 'production
     entry: {
-        'devtools/devtools': './src/devtools/devtools.js'
+        'devtools/panel': './src/devtools/panel.ts'
     },
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, 'dist'),
     },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+            },
+            {
+                test: /\.css$/i,
+                include: path.resolve(__dirname, 'src'),
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
+            },
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                loader: 'ts-loader'
+            },
+            {
+                test: /\.di.(yaml|yml|json)$/,
+                use: require.resolve('@enhavo/dependency-injection/service-loader')
+            },
+
+        ]
+    },
+    resolve: {
+        extensions: [
+            '.tsx', '.ts', '.js', '.vue', '.yaml'
+        ],
+    },
     plugins: [
+        new VueLoaderPlugin(),
         new CopyWebpackPlugin({
             patterns: [
                 {from: './manifest.json', to: '.'},
-                {from: './src/devtools/index.html', to: 'devtools'}
+                {
+                    from: 'src', to: '.', globOptions: {
+                        ignore: ['**/*.ts', '**/*.vue'],
+                        dot: true
+                    }
+                }
             ],
         }),
     ],
+    devtool: false
 };
