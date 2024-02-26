b840241f1f52d4efbde4b055dcde6ef0b6dbfcdd
Michele Capichioni <capimichi@gmail.com>
2024-02-26 21:33:37 +0100

    Refactor component import in devtools panel

--- src/devtools/panel.ts
+++ src/devtools/panel.ts
@@ -1,16 +1,15 @@
-import 'reflect-metadata';
 import '../assets/styles.css'
 import {createApp} from 'vue'
 import {createRouter, createWebHashHistory} from 'vue-router'
 import App from '../components/App.vue'
-import devToolIndex from '../components/devtools/DevToolIndex.vue'
+import DevToolIndex from '../components/devtools/DevToolIndex.vue'
 
 const router = createRouter({
     history: createWebHashHistory(),
     routes: [
         {
             path: '/',
-            component: devToolIndex
+            component: DevToolIndex
         }
     ]
 })
