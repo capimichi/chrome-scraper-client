9606a5217630549d38b2c4c20ca58086ceec0dcd
Michele Capichioni <capimichi@gmail.com>
2024-03-09 22:07:46 +0100

    Refactor devtools code

---
+++ src/devtools/index.ts
@@ -0,0 +1,6 @@
+import TabManager from "../manager/TabManager";
+
+
+chrome.devtools.panels.create('Scraper Client', 'icon.png', 'devtools/panel.html', (panel) => {
+
+});
--- src/devtools/panel.ts
+++ src/devtools/panel.ts
@@ -3,6 +3,11 @@ import {createApp} from 'vue'
 import {createRouter, createWebHashHistory} from 'vue-router'
 import App from '../components/App.vue'
 import DevToolIndex from '../components/devtools/DevToolIndex.vue'
+import TabManager from "../manager/TabManager";
+import RequestsManager from "../manager/RequestsManager";
+
+TabManager.getInstance();
+RequestsManager.getInstance();
 
 const router = createRouter({
     history: createWebHashHistory(),
